9fe4e8c54352b9981c6e3c8518adc00a298bb833
Miguel <dev.miguelsouto@gmail.com>
2024-08-17 14:36:36 -0300

    alpha zero

---
+++ pizzas.js
@@ -0,0 +1,98 @@
+let pizzaJson = [
+    {
+        id: 1,
+        name: 'Mussarela',
+        img: 'images/pizza.png',
+        price: [21.00, 26.00, 31.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Molho de tomate, camada dupla de mussarela e orégano'
+      },
+      {
+        id: 2,
+        name: 'Calabresa',
+        img: 'images/pizza2.png',
+        price: [23.00, 29.00, 35.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Molho de tomate, mussarela, calabresa fatiada, cebola e orégano'
+      },
+      {
+        id: 3,
+        name: 'Quatro Queijos',
+        img: 'images/pizza3.png',
+        price: [25.00, 32.00, 40.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Molho de tomate, camadas de mussarela, provolone, parmessão, gorgonzola e orégano'
+      },
+      {
+        id: 4,
+        name: 'Brasiliense',
+        img: 'images/pizza4.png',
+        price: [25.00, 35.00, 43.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Molho de tomate, mussarela, calabresa picada, palmito, champignon, azeitonas e orégano'
+      },
+      {
+        id: 5,
+        name: 'Portuguesa',
+        img: 'images/pizza5.png',
+        price: [25.00, 35.00, 43.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Molho de tomate, mussarela, presunto, ovos, cebolas, pimentão, azeitona  e orégano'
+      },
+      {
+        id: 6,
+        name: 'Moda da Casa',
+        img: 'images/pizza6.png',
+        price: [30.00, 36.00, 45.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Molho de tomate, mussarela, carne de sol, tomates em cubos, coentro, cebola, azeitona, catupiry e orégano'
+      },
+      {
+        id: 7,
+        name: 'Banana com canela',
+        img: 'images/pizza7.png',
+        price: [27.00, 30.00, 32.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Mussarela, banana, canela e açúcar'
+      },
+      {
+        id: 8,
+        name: 'Coca Cola',
+        img: 'images/coca.png',
+        price: [6.00, 10.00, 12.00],
+        sizes: [
+          '350 ml',
+          '1 L',
+          '1.5 L'
+        ],
+        description: 'Foi de pizza? Vem de Coca!'
+      }
+];
